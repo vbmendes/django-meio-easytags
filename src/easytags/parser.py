b14cb6c4e0b9ab47cdfed21250a4c4d5a34d9df4
Vinícius Mendes <vbmendes@gmail.com>
2011-02-20 12:37:47 -0300

    Method that parses the token.

---
+++ src/easytags/parser.py
@@ -0,0 +1,15 @@
+
+
+def get_args_kwargs_from_token_parse(parser, token):
+    bits = token.split_contents()
+    args = []
+    kwargs = {}
+
+    for bit in bits[1:]:
+        splitted_bit = bit.split('=')
+        if len(splitted_bit) > 1:
+            kwargs[splitted_bit[0]] = '='.join(splitted_bit[1:])
+        else:
+            args.append(bit)
+            
+    return {'args': tuple(args), 'kwargs': kwargs}
--- src/easytags/tests.py
+++ src/easytags/tests.py
@@ -1,4 +1,8 @@
+
+from django import template
 from django.test import TestCase
+
+from easytags.parser import get_args_kwargs_from_token_parse
 
 class EasyTagsTests(TestCase):
 
@@ -8,3 +12,21 @@ class EasyTagsTests(TestCase):
         """
         self.assertTrue(True)
 
+    def test_parse_tag_with_args(self):
+        """
+            Tests if the parser recognizes one tag.
+        """
+        parser = template.Parser([])
+        token = template.Token(template.TOKEN_BLOCK, 'tag_name arg1 arg2')
+        self.assertEquals(
+            {'args': ('arg1', 'arg2'), 'kwargs': {}},
+            get_args_kwargs_from_token_parse(parser, token)
+        )
+    
+    def test_parse_kwargs_tag(self):
+        parser = template.Parser([])
+        token = template.Token(template.TOKEN_BLOCK, 'tag_name arg1="1" arg2="2"')
+        self.assertEquals(
+            {'args': (), 'kwargs': {'arg1': '"1"', 'arg2': '"2"'}},
+            get_args_kwargs_from_token_parse(parser, token)
+        )
