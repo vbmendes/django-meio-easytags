006c2ed6e07859911c9d94f4d147bb522e083d18
Vinícius Mendes <vbmendes@gmail.com>
2011-02-20 11:57:24 -0300

    Testing if the environment is ok.

---
+++ src/easytags/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class EasyTagsTests(TestCase):
+
+    def test_environment(self):
+        """
+            Just make sure everything is set up correctly.
+        """
+        self.assertTrue(True)
+
