2b79d8aa999407e5a3d1cc0cdbdbb750b073e6c4
Vinícius Mendes <vbmendes@gmail.com>
2011-02-21 01:48:37 -0300

    Improving setup.py to put it in CheeseShop.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-meio-easytags",
@@ -6,9 +11,22 @@ setup(
     url = "http://github.com/vbmendes/django-meio-easytags",
     license = "BSD",
     description = "An easy way to create custom template tags for Django's templating system.",
+    long_description = read('README'),
     author = "Vinicius Mendes",
+    author_email = "vbmendes@gmail.com",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires= ['setuptools'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
 
