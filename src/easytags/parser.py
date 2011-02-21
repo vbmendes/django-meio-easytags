f794290836ca63623145fd76a32f286298785e94
Vinícius Mendes <vbmendes@gmail.com>
2011-02-20 22:40:35 -0300

    Refactoring to improve parser readability.

--- src/easytags/parser.py
+++ src/easytags/parser.py
@@ -9,18 +9,22 @@ Created on 20/02/2011
 from django.template import TemplateSyntaxError
 
 
+is_kwarg = lambda bit: not bit[0] in (u'"', u"'") and u'=' in bit
+
+
 def get_args_kwargs_from_token_parse(parser, token):
     bits = token.split_contents()
     args = []
     kwargs = {}
-
+    
     for bit in bits[1:]:
-        splitted_bit = bit.split(u'=')
-        if not bit[0] in (u'"', u"'") and len(splitted_bit) > 1:
+        if is_kwarg(bit):
+            splitted_bit = bit.split(u'=')
             kwargs[splitted_bit[0]] = u'='.join(splitted_bit[1:])
-        elif not kwargs:
-            args.append(u'='.join(splitted_bit))
         else:
-            raise TemplateSyntaxError(u"Args must be before kwargs.")
-            
+            if not kwargs:
+                args.append(bit)
+            else:
+                raise TemplateSyntaxError(u"Args must be before kwargs.")
+                
     return {'args': tuple(args), 'kwargs': kwargs}
