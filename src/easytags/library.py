c27550a97db3478a238b43b6dc47c8b18aff0388
Vinicius Mendes <vinicius.mendes@corp.globo.com>
2013-05-10 15:14:31 -0300

    PEP8

--- src/easytags/library.py
+++ src/easytags/library.py
@@ -10,6 +10,7 @@ from django.template import Library
 
 from node import EasyNode, EasyAsNode
 
+
 class EasyLibrary(Library):
 
     @classmethod
@@ -18,14 +19,13 @@ class EasyLibrary(Library):
             renderer = name
             name = renderer.__name__
         return name, renderer
-    
-    def easytag(self, name = None, renderer = None):
+
+    def easytag(self, name=None, renderer=None):
         return self._handle_decorator(EasyNode, name, renderer)
-        
-    def easyastag(self, name = None, renderer = None):
+
+    def easyastag(self, name=None, renderer=None):
         return self._handle_decorator(EasyAsNode, name, renderer)
-        
-    
+
     def _handle_decorator(self, node_class, name, renderer):
         if not name and not renderer:
             return self.easytag
@@ -38,17 +38,17 @@ class EasyLibrary(Library):
                     return self._register_easytag(node_class, name, renderer)
                 return dec
         return self._register_easytag(node_class, name, renderer)
-    
+
     def _register_easytag(self, node_class, name, renderer):
         if not renderer:
             renderer = name
             name = renderer.__name__
-        
+
         def render_context(self, context, *args, **kwargs):
             return renderer(context, *args, **kwargs)
-        
+
         get_argspec = classmethod(lambda cls: node_class.get_argspec(renderer))
-        
+
         tag_node = type('%sEasyNode' % name, (node_class,), {
             'render_context': render_context,
             'get_argspec': get_argspec,
